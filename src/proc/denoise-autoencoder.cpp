63404cf01c424874a1f284eba83fb8e333e2febd
Noha Yassin <37175819+nohayassin@users.noreply.github.com>
2020-08-24 09:57:20 +0300

    denoise autoencoder integration

--- src/proc/denoise-autoencoder.cpp
+++ src/proc/denoise-autoencoder.cpp
@@ -16,11 +16,32 @@ namespace librealsense
 
     denoise_autoencoder::denoise_autoencoder() :
         depth_processing_block("Denoise Autoencoder"),
-        _width(0), _height(0), _stride(0)
+        _width(0), _height(0), _stride(0), _bpp(0),
+        _extension_type(RS2_EXTENSION_DEPTH_FRAME),
+        _current_frm_size_pixels(0)
     {
 
     }
 
+    rs2::frame denoise_autoencoder::process_frame(const rs2::frame_source& source, const rs2::frame& f)
+    {
+        update_configuration(f);
+        auto tgt = prepare_target_frame(f, source);
 
+        return tgt;
+    }
 
+    void  denoise_autoencoder::update_configuration(const rs2::frame& f)
+    {
+       
+    }
+
+    rs2::frame denoise_autoencoder::prepare_target_frame(const rs2::frame& f, const rs2::frame_source& source)
+    {
+        // Allocate and copy the content of the input data to the target
+        rs2::frame tgt = source.allocate_video_frame(_target_stream_profile, f, int(_bpp), int(_width), int(_height), int(_stride), _extension_type);
+
+        memmove(const_cast<void*>(tgt.get_data()), f.get_data(), _current_frm_size_pixels * _bpp);
+        return tgt;
+    }
 }
--- src/proc/denoise-autoencoder.h
+++ src/proc/denoise-autoencoder.h
@@ -9,9 +9,20 @@ namespace librealsense
     public:
         denoise_autoencoder();
 
+    protected:
+        void update_configuration(const rs2::frame& f);
+        rs2::frame process_frame(const rs2::frame_source& source, const rs2::frame& f) override;
+        rs2::frame prepare_target_frame(const rs2::frame& f, const rs2::frame_source& source);
+
     private:
 
         size_t                  _width, _height, _stride;
+        size_t                  _bpp;
+        rs2_extension           _extension_type;            // Strictly Depth/Disparity
+        size_t                  _current_frm_size_pixels;
+        rs2::stream_profile     _source_stream_profile;
+        rs2::stream_profile     _target_stream_profile;
+        uint8_t                 _hole_filling_mode;
     };
     MAP_EXTENSION(RS2_EXTENSION_DENOISE_AUTOENCODER, librealsense::denoise_autoencoder);
 }
