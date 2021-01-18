0c5a9cc361785ceec4721605f024e7cb896c3e36
Noha Yassin <37175819+nohayassin@users.noreply.github.com>
2021-01-18 11:00:28 +0200

    pipe - internal test for delay issue DSO-16305

--- unit-tests/internal/internal-tests-extrinsic.cpp
+++ unit-tests/internal/internal-tests-extrinsic.cpp
@@ -139,29 +139,23 @@ TEST_CASE("Pipe - Extrinsic memory leak detection", "[live]")
 
         auto video = mode.as<rs2::video_stream_profile>();
         auto res = configure_all_supported_streams(dev, video.width(), video.height(), mode.fps());
-        // 1. check if extrinsics table size is perserved over iterations of same stream type and fps
-        // 2. check if time to first frame exceeds a define threshold :
-        //      - RGB : 1200 msec
-        //      - DEPTH : (should be same as L500)
-        //      - IR : ?
-        // 3. check if there is increment of time to first frame: 
-        //      - throw exception even though the threshold from #2 is not reached
-        //      - run 20 iterations to get to this conclusion
-        //      - in each iteration check delay against the previous 2 iterations : 
-        //      - if current itration delay > previous 2 iterations delay, increase count by 1
 
-        struct time_increment
-        {
-            size_t t0 = 0;
-            size_t t1 = 0;
-            size_t count = 0;
-        };
+        // collect a log that contains info about 20 iterations for each stream
+        // the info should include:
+        // 1. extrinsics table size
+        // 2. delay to first frame
+        // 3. delay threshold for each stream (set fps=6 delay as worst case)
+        // the test will succeed only if all 3 conditions are met:
+        // 1. extrinsics table size is perserved over iterations for each stream 
+        // 2. no delay increment over iterations
+        // 3. "most" iterations have time to first frame delay below a defined threshold
 
-        std::map<std::string, size_t> extrinsic_graph_at_cfg;
-        std::map<std::string, time_increment> time_increment_at_cfg;
+
+        std::map<std::string, std::vector<size_t>> extrinsic_graph_at_cfg;
+        std::map<std::string, std::vector<size_t>> time_increment_at_cfg; // map to vector to collect all data
         std::map<size_t, size_t> delay_threshold_at_stream_type;
 
-        // TODO : set correct values for thresholds
+        // TODO : set correct values for thresholds (take threshold of fps=6)
         delay_threshold_at_stream_type[RS2_STREAM_DEPTH] = 6000;
         delay_threshold_at_stream_type[RS2_STREAM_COLOR] = 6000;
         delay_threshold_at_stream_type[RS2_STREAM_INFRARED] = 6000;
@@ -172,71 +166,78 @@ TEST_CASE("Pipe - Extrinsic memory leak detection", "[live]")
 
         std::map<std::string, size_t> extrinsic_graph_at_sensor;
         auto& b = environment::get_instance().get_extrinsics_graph();
-
+        rs2::config cfg;
         for (auto profile : res.second)
         {
+            cfg.enable_stream(profile.stream, profile.index, profile.width, profile.height, profile.format, profile.fps); // all streams in cfg
+
             int type = profile.stream;
             int format = profile.format;
             std::cout << "==================================================================================" << std::endl;
             std::cout << "stream type :" << type << ", index : " << profile.index << ", width : " << profile.width << ", height : " << profile.height << ", format : " << format << ", fps : " << profile.fps << std::endl;
             std::string cfg_key = std::to_string(format) + "," + std::to_string(profile.fps);
             std::cout << "cfg key :" << cfg_key << std::endl;
-            time_increment_at_cfg[cfg_key].count = 0;
-            time_increment_at_cfg[cfg_key].t0 = 0;
-            time_increment_at_cfg[cfg_key].t1 = 0;
 
-            for (auto i = 0; i < ITERATIONS_PER_CONFIG; i++)
+            //frames_per_iter_at_cfg[cfg_key] = profile.fps * 5;
+        }
+        //for (auto i = 0; i < ITERATIONS_PER_CONFIG; i++)
+        auto frames_per_iteration = 30 * 5; // fps*5
+        while (1) // break only when collecting 20 iterations for each stream (cfg)
+        {
+            rs2::config tmp_cfg = cfg;
+            rs2::pipeline pipe;
+            rs2::frameset data;
+            pipe.start(tmp_cfg);
+
+            try
             {
-                rs2::pipeline pipe;
-                rs2::config cfg;
-                cfg.enable_stream(profile.stream, profile.index, profile.width, profile.height, profile.format, profile.fps);
-                pipe.start(cfg);
-                auto frames_per_iteration = profile.fps * 5;
-
-                try
+                auto t1 = std::chrono::system_clock::now();
+                // TODO : use callback for this
+                for (auto i = 0; i < frames_per_iteration; i++)
                 {
-                    auto t1 = std::chrono::system_clock::now();
-                    for (auto i = 0; i < frames_per_iteration; i++)
-                    {
-                        rs2::frameset data = pipe.wait_for_frames(); // Wait for next set of frames from the camera
-                    }
-
-                    auto t2 = std::chrono::system_clock::now();
-                    auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
-
-
-                    // 1. check extrinsics table size
-                    if (!extrinsic_graph_at_cfg.count(cfg_key))
-                    {
-                        extrinsic_graph_at_cfg[cfg_key] = b._extrinsics.size();
-                        std::cout << " Extrinsic Graph size is " << extrinsic_graph_at_cfg[cfg_key] << ", Time to first frame is " << diff;// << std::endl;
-                    }
-                    else {
-                        std::cout << " Extrinsic Graph size is " << extrinsic_graph_at_cfg[cfg_key] << ", Time to first frame is " << diff;// << std::endl;
-                        REQUIRE(b._extrinsics.size() == extrinsic_graph_at_cfg[cfg_key]);
-                    }
-
-                    // 2. threshold 
-                    REQUIRE(diff < delay_threshold_at_stream_type[profile.stream]);
-
-                    // 3. delay increment
-                    if (diff > time_increment_at_cfg[cfg_key].t0 && diff > time_increment_at_cfg[cfg_key].t1)
-                    {
-                        time_increment_at_cfg[cfg_key].count += 1;
-                        REQUIRE(time_increment_at_cfg[cfg_key].count < TIME_INCREMENT_THRESHOLD);
-                    }
-                    std::cout << ", increment count is: " << time_increment_at_cfg[cfg_key].count << std::endl;
-                    // cache only previous 2 iterations
-                    time_increment_at_cfg[cfg_key].t0 = time_increment_at_cfg[cfg_key].t1;
-                    time_increment_at_cfg[cfg_key].t1 = diff;
-
-                    pipe.stop();
+                    data = pipe.wait_for_frames(); // Wait for next set of frames from the camera
                 }
-                catch (...)
+                //data.fp
+                auto t2 = std::chrono::system_clock::now();
+                auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
+
+                auto color_frame = data.get_color_frame();
+                auto depth_frame = data.get_depth_frame();
+                auto ir_frame = data.get_infrared_frame();
+                auto fisheye_frame = data.get_fisheye_frame();
+                auto sensor = data.get_sensor();
+                auto profile = data.get_profile();
+                // 1. check extrinsics table size
+               /* if (!extrinsic_graph_at_cfg.count(cfg_key))
                 {
-                    std::cout << "Iteration failed  " << std::endl;
-                    exit(EXIT_FAILURE);
+                    extrinsic_graph_at_cfg[cfg_key] = b._extrinsics.size();
+                    std::cout << " Extrinsic Graph size is " << extrinsic_graph_at_cfg[cfg_key] << ", Time to first frame is " << diff;// << std::endl;
                 }
+                else {
+                    std::cout << " Extrinsic Graph size is " << extrinsic_graph_at_cfg[cfg_key] << ", Time to first frame is " << diff;// << std::endl;
+                    REQUIRE(b._extrinsics.size() == extrinsic_graph_at_cfg[cfg_key]);
+                }
+
+                // 2. threshold 
+                REQUIRE(diff < delay_threshold_at_stream_type[profile.stream]);
+
+                // 3. delay increment
+                if (diff > time_increment_at_cfg[cfg_key].t0 && diff > time_increment_at_cfg[cfg_key].t1)
+                {
+                    time_increment_at_cfg[cfg_key].count += 1;
+                    REQUIRE(time_increment_at_cfg[cfg_key].count < TIME_INCREMENT_THRESHOLD);
+                }
+                std::cout << ", increment count is: " << time_increment_at_cfg[cfg_key].count << std::endl;
+                // cache only previous 2 iterations
+                time_increment_at_cfg[cfg_key].t0 = time_increment_at_cfg[cfg_key].t1;
+                time_increment_at_cfg[cfg_key].t1 = diff;
+                */
+                pipe.stop();
+            }
+            catch (...)
+            {
+                std::cout << "Iteration failed  " << std::endl;
+                exit(EXIT_FAILURE);
             }
         }
     }
