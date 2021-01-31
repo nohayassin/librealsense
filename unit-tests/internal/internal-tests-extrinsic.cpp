5b3fa8a28c2b4c7e27f255aabc5e023fb4416928
Noha <noha.yassin@gmail.com>
2021-01-31 17:53:11 +0200

    D455 frames drop test

--- unit-tests/internal/internal-tests-extrinsic.cpp
+++ unit-tests/internal/internal-tests-extrinsic.cpp
@@ -11,12 +11,13 @@
 #include <librealsense2/hpp/rs_sensor.hpp>
 #include "../../common/tiny-profiler.h"
 #include "./../src/environment.h"
+#include <unit-tests-common.h>
 
 using namespace librealsense;
 using namespace librealsense::platform;
 
 // Require that vector is exactly the zero vector
-inline void require_zero_vector(const float(&vector)[3])
+/*inline void require_zero_vector(const float(&vector)[3])
 {
     for (int i = 1; i < 3; ++i) REQUIRE(vector[i] == 0.0f);
 }
@@ -27,7 +28,7 @@ inline void require_identity_matrix(const float(&matrix)[9])
     static const float identity_matrix_3x3[] = { 1,0,0, 0,1,0, 0,0,1 };
     for (int i = 0; i < 9; ++i) REQUIRE(matrix[i] == approx(identity_matrix_3x3[i]));
 }
-
+*/
 TEST_CASE("Extrinsic graph management", "[live][multicam]")
 {
     // Require at least one device to be plugged in
@@ -92,3 +93,107 @@ TEST_CASE("Extrinsic graph management", "[live][multicam]")
         WARN("TODO: Graph size shall be preserved: init " << init_size << " != final " << end_size);
     }
 }
+TEST_CASE("D455 Frame Drops", "[live]")
+{
+    // Require at least one device to be plugged in
+
+    //rs2::context ctx;
+    //if (make_context(SECTION_FROM_TEST_NAME, &ctx))
+    //{
+    rs2::context ctx;
+    {
+        rs2::log_to_file(RS2_LOG_SEVERITY_DEBUG, "lrs_log.txt");
+
+        std::cout << "D455 Frame Drops started" << std::endl;
+        for (auto i = 0; i < 1; i++)
+                    {
+            auto list = ctx.query_devices();
+            REQUIRE(list.size());
+            auto dev = list.front();
+            auto sensors = dev.query_sensors();
+
+
+            //bool usb3_device = is_usb3(dev);
+            int fps = 90; //usb3_device ? 90 : 15; // In USB2 Mode the devices will switch to lower FPS rates
+            int req_fps = 90;//usb3_device ? 90 : 30; // USB2 Mode has only a single resolution for 60 fps which is not sufficient to run the test
+
+            int width = 640;
+            int height = 360;
+
+            auto res = configure_all_supported_streams(dev, width, height, fps);
+            for (auto& s : res.first) s.close();
+
+            rs2::config cfg;
+            rs2::pipeline pipe;
+            size_t cfg_size = 0;
+            rs2::sensor ss;
+
+            std::vector<rs2::stream_profile> sensor_stream_profiles;
+            for (auto& profile : res.second)
+            {
+                auto fps = profile.fps;
+                if (profile.stream != RS2_STREAM_COLOR) continue;
+                cfg.enable_stream(profile.stream, profile.index, profile.width, profile.height, profile.format, fps); // all streams in cfg
+                cfg_size += 1;
+                // create stream profiles data structure to open streams per sensor when testing in sensor mode
+                for (auto& s : res.first)
+                {
+                    auto stream_profiles = s.get_stream_profiles();
+                    for (auto& sp : stream_profiles)
+                    {
+                        if (sp.stream_type() != RS2_STREAM_COLOR) continue;
+                        if (!(sp.stream_type() == profile.stream && sp.fps() == fps && sp.stream_index() == profile.index && sp.format() == profile.format)) continue;
+
+                        auto vid = sp.as<rs2::video_stream_profile>();
+                        auto h = vid.height();
+                        auto w = vid.width();
+                        if (!(w == profile.width && h == profile.height)) continue;
+                        sensor_stream_profiles.push_back(sp);
+                        ss = s;
+                    }
+                }
+            }
+
+                auto start_time = std::chrono::system_clock::now().time_since_epoch();
+                auto start_time_milli = std::chrono::duration_cast<std::chrono::milliseconds>(start_time).count();
+                std::mutex mutex;
+                std::mutex mutex_2;
+                auto process_frame = [&](const rs2::frame& f)
+                {
+                    auto now = std::chrono::system_clock::now().time_since_epoch();
+                    auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(now - start_time).count();
+                    std::lock_guard<std::mutex> lock(mutex_2);
+                    auto stream_type = f.get_profile().stream_name();
+                    auto frame_num = f.get_frame_number();
+                    auto time_of_arrival = f.get_frame_metadata(RS2_FRAME_METADATA_TIME_OF_ARRIVAL);
+                    std::cout << "frame_num = "<< frame_num <<", time_of_arrival diff=" << diff<<std::endl;
+
+                };
+                auto frame_callback = [&](const rs2::frame& f)
+                {
+                    std::lock_guard<std::mutex> lock(mutex);
+                    if (rs2::frameset fs = f.as<rs2::frameset>())
+                    {
+                        // With callbacks, all synchronized stream will arrive in a single frameset
+                        for (const rs2::frame& ff : fs)
+                        {
+                            process_frame(ff);
+                        }
+                    }
+                    else
+                    {
+                        // Stream that bypass synchronization (such as IMU) will produce single frames
+                        process_frame(f);
+                    }
+                };
+
+                ss.open(sensor_stream_profiles);
+                ss.start(frame_callback);
+                std::this_thread::sleep_for(std::chrono::seconds(2));
+                ss.stop();
+
+                ss.close();
+        }
+}
+}
+
