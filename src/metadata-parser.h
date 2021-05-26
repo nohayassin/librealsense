802e31b6c480bbde1ecd875cf90dace2249f4d32
Noha Yassin <37175819+nohayassin@users.noreply.github.com>
2021-05-26 09:34:47 +0300

    added metadata parser for depth units - WIP

--- src/metadata-parser.h
+++ src/metadata-parser.h
@@ -454,4 +454,49 @@ namespace librealsense
         std::shared_ptr<md_sr300_attribute_parser<S, Attribute>> parser(new md_sr300_attribute_parser<S, Attribute>(attribute, offset, mod));
         return parser;
     }
+
+    // Depth Units Parser
+    template<class S, class Attribute>
+    class md_depth_units_parser : public md_attribute_parser_base
+    {
+    public:
+        md_depth_units_parser(Attribute S::* attribute_name, unsigned long long offset, attrib_modifyer mod) :
+            _md_attribute(attribute_name), _offset(offset), _modifyer(mod) {};
+
+        rs2_metadata_type get(const librealsense::frame& frm) const override
+        {
+            if (!supports(frm))
+                throw invalid_value_exception("Metadata is not available");
+
+            auto s = reinterpret_cast<const S*>((frm.additional_data.metadata_blob.data()) + _offset);
+
+            auto param = static_cast<rs2_metadata_type>((*s).*_md_attribute);
+            if (_modifyer)
+                param = _modifyer(param);
+
+            return param;
+        }
+
+        bool supports(const frame& frm) const override
+        {
+            return true; // TODO : check if expression is needed
+        }
+
+    private:
+        md_depth_units_parser() = delete;
+        md_depth_units_parser(const md_depth_units_parser&) = delete;
+
+        Attribute S::* _md_attribute;  // Pointer to the actual data field
+        unsigned long long  _offset;        // Inner struct offset with regard to the most outer one
+        attrib_modifyer     _modifyer;      // Post-processing on received attribute
+    };
+
+    /**\brief A helper function to create a specialized attribute parser.
+    *  Return it as a pointer to a base-class*/
+    template<class S, class Attribute>
+    std::shared_ptr<md_attribute_parser_base> make_depth_units_parser(Attribute S::* attribute, unsigned long long offset, attrib_modifyer  mod = nullptr)
+    {
+        std::shared_ptr<md_depth_units_parser<S, Attribute>> parser(new md_depth_units_parser<S, Attribute>(attribute, offset, mod));
+        return parser;
+    }
 }
